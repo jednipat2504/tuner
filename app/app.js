c00b8f5063d22676932e660b665cdaa76fe4470a
jednipat2504 <104492366+jednipat2504@users.noreply.github.com>
2024-08-27 17:14:49 +0700

    Update app.js

--- app/app.js
+++ app/app.js
@@ -32,7 +32,7 @@ Application.prototype.start = function () {
     }
   };
 
-  swal.fire("Chedni Tuner").then(function () {
+  swal.fire("ChedniTuner").then(function () {
     self.tuner.init();
     self.frequencyData = new Uint8Array(self.tuner.analyser.frequencyBinCount);
   });
